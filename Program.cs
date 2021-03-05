10e37d2a4083581f591b7e9af39e8fe16c0fe179
LipatovValentin <ValentinLipatov@bk.ru>
2021-03-05 18:51:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var data = Test1(0);
+            foreach (var element in Test1(1))
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
+        }
+        public static IEnumerable<int> Test1(int value)
+        {
+            if (value == 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                return Test2();
+            }
+        }
+        public static IEnumerable<int> Test2()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+    }
+}
